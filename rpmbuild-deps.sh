5ae6a8c1d16a8a52b189ba5cb717a3ed529166cd
J.F.Gratton <jean-francois@famillegratton.net>
2023-11-18 20:15:20 -0500

    rpmbuild fixes

--- rpmbuild-deps.sh
+++ rpmbuild-deps.sh
@@ -5,11 +5,11 @@ grep ^BuildRequires "dtools.spec" |awk -F\: '{print "sudo dnf install -y"$2}'|se
 echo;echo;echo "Done. Now installing the Go binaries"
 
 echo "Fetching archive..."
-sudo wget -q https://go.dev/dl/go1.21.4.linux-amd64.tar.gz -O /tmp/go.tar.gz
+wget -q https://go.dev/dl/go1.21.4.linux-amd64.tar.gz -O /tmp/go.tar.gz
 # sudo wget -q https://go.dev/dl/go1.21.4.linux-x86_64.tar.gz -O /tmp/go.tar.gz -O /opt/go.tar.gz
 
 echo "Unarchiving..."
-cd /opt ; sudo rm -rf go;sudo tar zxf go.tar.gz; sudo rm -f go.tar.gz
+cd /opt ; sudo rm -rf go;sudo tar zxf /tmp/go.tar.gz; rm -f /tmp/go.tar.gz
 
 echo "Completed."
 
