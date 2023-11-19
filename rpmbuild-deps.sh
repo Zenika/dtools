38692210e83febefab396b82eb4962b4733793f0
RPM Builder <builder@famillegratton.net>
2023-11-18 20:22:38 -0500

    ÃFixed rpm build deps

--- rpmbuild-deps.sh
+++ rpmbuild-deps.sh
@@ -6,7 +6,6 @@ echo;echo;echo "Done. Now installing the Go binaries"
 
 echo "Fetching archive..."
 wget -q https://go.dev/dl/go1.21.4.linux-amd64.tar.gz -O /tmp/go.tar.gz
-# sudo wget -q https://go.dev/dl/go1.21.4.linux-x86_64.tar.gz -O /tmp/go.tar.gz -O /opt/go.tar.gz
 
 echo "Unarchiving..."
 cd /opt ; sudo rm -rf go;sudo tar zxf /tmp/go.tar.gz; rm -f /tmp/go.tar.gz
