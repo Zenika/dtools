de6ae006ca34c46aa8dd14c47987538cfd944a4c
jeanfrancoisgratton <jean-francois@famillegratton.net>
2023-12-23 16:25:11 -0500

    Extra commands stubs

---
+++ src/extras/dockerclean.go
@@ -0,0 +1,6 @@
+// dtools
+// Written by J.F. Gratton <jean-francois@famillegratton.net>
+// Original filename: src/extras/dockerclean.go
+// Original timestamp: 2023/12/23 15:55
+
+package extras
---
+++ src/extras/dockerrm.go
@@ -0,0 +1,6 @@
+// dtools
+// Written by J.F. Gratton <jean-francois@famillegratton.net>
+// Original filename: src/extras/dockerrm.go
+// Original timestamp: 2023/12/23 15:55
+
+package extras
